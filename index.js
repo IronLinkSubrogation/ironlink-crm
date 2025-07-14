49f7517bba415672ec081397f5d4bb9b329ff046
IronLinkSubrogation <ironlinksubrogation@gmail.com>
2025-07-13 19:38:52 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from /public
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+// Root route just for sanity check
+app.get('/', (req, res) => {
+  res.send('IronLink CRM is up and running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is listening on port ${PORT}`);
+});
