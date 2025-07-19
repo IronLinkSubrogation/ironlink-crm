381a3c0d4868ef12782e6783115413dd7891e7aa
IronLinkSubrogation <ironlinksubrogation@gmail.com>
2025-07-19 08:25:14 -0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -9,9 +9,20 @@ const {
   deleteUser,
 } = require("../controllers/userController");
 
+// GET /users
+// Admin-only: Returns list of all users
 router.get("/", verifyToken, checkRole("admin"), getAllUsers);
+
+// POST /users
+// Admin-only: Creates a new user
 router.post("/", verifyToken, checkRole("admin"), createUser);
+
+// PUT /users/:id
+// Admin-only: Updates user by ID
 router.put("/:id", verifyToken, checkRole("admin"), updateUser);
+
+// DELETE /users/:id
+// Admin-only: Deletes user by ID
 router.delete("/:id", verifyToken, checkRole("admin"), deleteUser);
 
 module.exports = router;
